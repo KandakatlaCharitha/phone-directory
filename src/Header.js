d489a4e5f6fdad601f1d5411e27d0c280d63471a
Kandakatla Charitha <kandakatlacharithareddy@gmail.com>
2024-05-20 19:27:31 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,19 @@
 import React from "react";
+
 const Header = function () {
-  return <div className="header">Phone Directory</div>;
+  const headerStyle = {
+    textAlign: "center",
+    padding: 20,
+    background: "#000",
+    color: "#fff",
+    textTransform: "uppercase",
+  };
+
+  return (
+    <div style={headerStyle}>
+      <div className="header">Phone Directory</div>
+    </div>
+  );
 };
 
 export default Header;
